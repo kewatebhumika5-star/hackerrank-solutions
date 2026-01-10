5016a795d188342585ca5e6bb77b7bdf61f2494c
Bhumika Kewate <kewatebhumika5@gmail.com>
2026-01-10 20:07:07 +0530

    Solved recursive series using sum of previous three terms in C

---
+++ RecursiveSeries.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int series(int n, int a, int b, int c) {
+    if (n == 1)
+        return a;
+    if (n == 2)
+        return b;
+    if (n == 3)
+        return c;
+
+    return series(n - 1, a, b, c)
+         + series(n - 2, a, b, c)
+         + series(n - 3, a, b, c);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("%d", series(n, a, b, c));
+
+    return 0;
+}
